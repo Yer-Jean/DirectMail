6521f037dc0d72483efb1f3cea0182279c52eaf4
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-10-27 13:51:57 +0600

    Login-SignUp with email activation added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
+from django.utils.crypto import get_random_string
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm  # Переопределяем стандартную форму UserCreationForm на свою
+    template_name = 'users/signup.html'
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.verification_token = get_random_string(30)
+        send_mail(
+            subject='Activate Your Account Now',
+            message=f'Hello!\nPlease, click link below to confirm your email\n'
+                    f'http://{get_current_site(self.request)}/users/confirm/{self.object.verification_token}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[self.object.email]
+        )
+        return super().form_valid(form)
+
+
+def activate_user(request, token):
+    user = User.objects.get(verification_token=token)
+    user.verification_token = ''
+    user.is_active = True
+    user.save()
+    return redirect(reverse('users:login'))
