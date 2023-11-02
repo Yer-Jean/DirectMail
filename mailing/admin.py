916d4e44a81e4bc73e6dbbd9159eb90d48096786
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-11-02 20:29:36 +0600

    CRUD for messages realized

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from mailing.models import Address
+from mailing.models import Address, Message
 
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email_address', 'phone_number',
-                    'gender', 'birthday', 'comment', 'is_correct', 'created_date', 'modified_date')
+    list_display = ('first_name', 'last_name', 'email_address', 'is_correct',
+                    'created_by', 'created_date', 'modified_date')
+    list_filter = ('created_date',)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('description', 'subject', 'text_message', 'created_by', 'created_date', 'modified_date')
     list_filter = ('created_date',)
--- mailing/urls.py
+++ mailing/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from mailing.apps import MailingConfig
 from mailing.views import IndexView, AddressListView, AddressCreateView, AddressDetailView, AddressUpdateView, \
-    AddressDeleteView
+    AddressDeleteView, MessageListView, MessageCreateView, MessageDetailView, MessageUpdateView, MessageDeleteView
 
 app_name = MailingConfig.name
 
@@ -14,4 +14,10 @@ urlpatterns = [
     path('addresses/view/<int:pk>/', AddressDetailView.as_view(), name='address_view'),
     path('addresses/update/<int:pk>', AddressUpdateView.as_view(), name='address_update'),
     path('addresses/delete/<int:pk>', AddressDeleteView.as_view(), name='address_delete'),
+
+    path('messages/', MessageListView.as_view(), name='messages'),
+    path('messages/create/', MessageCreateView.as_view(), name='message_create'),
+    path('messages/view/<int:pk>/', MessageDetailView.as_view(), name='message_view'),
+    path('messages/update/<int:pk>', MessageUpdateView.as_view(), name='message_update'),
+    path('messages/delete/<int:pk>', MessageDeleteView.as_view(), name='message_delete'),
 ]
