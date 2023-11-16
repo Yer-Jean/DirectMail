d32976d7ebda52c6066cc7c0e5bfdf5eaaf93756
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-11-17 00:12:40 +0600

    Template of Blog app created

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+# from django.urls import path
+#
+# from .apps import BlogConfig
+# from .views import ArticleListView
+#
+# app_name = BlogConfig.name
+#
+# urlpatterns = [
+#     path('articles/', ArticleListView.as_view(), name='articles'),
+#     # path('addresses/create/', AddressCreateView.as_view(), name='address_create'),
+#     # path('addresses/view/<int:pk>/', AddressDetailView.as_view(), name='address_view'),
+#     # path('addresses/update/<int:pk>', AddressUpdateView.as_view(), name='address_update'),
+#     # path('addresses/delete/<int:pk>', AddressDeleteView.as_view(), name='address_delete'),
+# ]
---
+++ blog/views.py
@@ -0,0 +1,9 @@
+# from django.shortcuts import render
+# from django.views.generic import ListView
+#
+# from blog.models import Article
+#
+#
+# class ArticleListView(ListView):
+#     model = Article
+#
